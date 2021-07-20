6f8964e0f4f5f661e8b676990c455e06464a8afc
504 <504@DESKTOP-B0KVD9C>
2021-07-20 17:59:23 +0900

    chap08

---
+++ chap01/src/chap08/RemoteControlExample.java
@@ -0,0 +1,18 @@
+package chap08;
+
+public class RemoteControlExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RemoteControl rc = null;
+				
+		rc= new Television();
+		rc.turnOn();
+		rc.setMute(false);
+		
+		rc = new Audio();
+		rc.turnOn();
+		rc.setMute(true);
+	}
+
+}
